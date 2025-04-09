e9ff0fae203859c7785241428570006aad8feb50
Matt Morris <piemashandgravy@gmai.com>
2025-04-09 14:02:53 +0100

    fallingobject.py v2

--- fallingobject.py
+++ fallingobject.py
@@ -6,10 +6,17 @@ Created on Wed Apr  9 08:21:28 2025
 """
 
 from numpy import sqrt
+from pint import UnitRegistry
+
+units = UnitRegistry()
+metre = units.metre
+second = units.second
+a = 9.8 * metre / second**2
+
 
 
 u = 0                     # initial velocity
-a = 9.8                   # acceleration due to gravity
+# a = 9.8                   # acceleration due to gravity
 t = 3.4                   # time in seconds
 
 v = u + a*t               # final velocity = iniial velocity + velocity due to acceleration over time t
@@ -20,12 +27,18 @@ h = 381                   # height of building
 
 
 
-def DistGivenTime(t, u=0, a=9.8):
+def DistGivenTime(t, u=0):
     
     return u*t + a*t**2 / 2 
 
 
 
-def TimeGivenDist(x, u=0, a=9.8):
+def TimeGivenDist(x, u=0):
     
-    return float(-1 * (-u + sqrt(u**2 + 2*a*x)) / a)
+    return -1 * (-u + sqrt(u**2 + 2*a*x)) / a
+
+
+
+def VelocityOnImpact(t, u=0):
+    
+    return u + a*t
